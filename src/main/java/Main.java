0bd27cf8f3b66dc98ffe29c38e3718b7ec442430
Yra <scientia44@gmail.com>
2018-05-27 07:43:07 +0300

    New task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Future project");
+        int a=266;
+        byte b=(byte)a;
+        System.out.println(b);
     }
 }
